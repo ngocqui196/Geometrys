4a1d17fb45df848966c1a9c2a052bc7592c989f4
ngocqui1996 <hivongmotloihua@yahoo.com>
2020-07-29 22:45:25 +0700

    sua lai

--- src/DisplayGeometry.java
+++ src/DisplayGeometry.java
@@ -15,14 +15,65 @@ public class DisplayGeometry {
                     int width = sc.nextInt();
                     System.out.println("The height: ");
                     int height = sc.nextInt();
-                    for (int i = 0; i < width; i++) {
-                        System.out.println("\n");
-                        for (int j = 0; j < height; j++) {
+                    System.out.println("Draw the square");
+                    for (int i = 0; i < height; i++) {
+                        System.out.println();
+                        for (int j = 0; j < width; j++) {
                             System.out.print("*");
                         }
                     }
                     break;
                 case 2:
+
+                    System.out.println("The width: ");
+                    int widthTrianger = sc.nextInt();
+                    System.out.println("The height: ");
+                    int heightTrianger = sc.nextInt();
+                    System.out.println("1. Right triangle botton-left");
+                    System.out.println("2. Right triangle top-left");
+                    int selection = sc.nextInt();
+                    switch (selection){
+                        case 1:
+                            System.out.println("Draw the trianger botton-left!");
+                            for (int i = 0; i < heightTrianger; i++) {
+                                System.out.println();
+                                for (int j = 0; j < widthTrianger; j++) {
+                                    if (j<=i){
+                                        System.out.print("*");
+                                    }
+                                }
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Draw the trianger top-left!");
+                            for (int i = 0; i < heightTrianger; i++) {
+                                System.out.println();
+                                for (int j = widthTrianger; j > 0; j--) {
+                                    if (j > i) {
+                                        System.out.print("*");
+                                    }
+                                }
+                            }
+                    }
+                    break;
+                case 3:
+                    System.out.println("The height : ");
+                    int heightRightTrianger = sc.nextInt();
+                    System.out.println("The width: ");
+                    int widthRightTrianger = sc.nextInt();
+                    for (int i = 1; i <= widthRightTrianger; i++) {
+                        System.out.println();
+                        for (int j = 1; j <= heightRightTrianger; j++) {
+                            if (j <= heightRightTrianger/2 - i || j >= heightRightTrianger/2 +i){
+                                System.out.print(" ");
+                            }else{
+                                System.out.print("*");
+                            }
+                        }
+                    }
+                    break;
+                case 0 :
+                System.exit(0);
 
             }
         }
